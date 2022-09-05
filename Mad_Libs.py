f4dd47b5bb91c3a9e9f07efcd419c9fdc9d7ef57
Aayush3014 <0201csai036@niet.co.in>
2022-09-05 21:15:21 +0530

    Initial Commit

---
+++ Mad_Libs.py
@@ -0,0 +1,8 @@
+# Let the user enter the nouns adjectives pronouns etc. and complete a story.
+
+color = input("Enter color")
+work = input("Enter work")
+time = input("Enter Time")
+print("Roses are " + color)
+print("I have to do some " + work)
+print("I have to do "+time)
